1dc44d796fb60ac65d9ed416f92702d38165db24
Ziyang Hu <hu.ziyang@cantab.net>
2019-03-27 14:25:56 +0800

    compilation on windows

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['torch_asg'],
     ext_modules=[CppExtension(name='torch_asg_native',
                               sources=['torch_asg/native/torch_asg.cpp'],
-                              extra_compile_args=['-Wno-sign-compare', '-fopenmp']),
+                              extra_compile_args=['-fopenmp']),
                  # CUDAExtension(name='torch_asg_cuda',
                  #               sources=['torch_asg/native/torch_asg_cuda.cpp',
                  #                        'torch_asg/native/torch_asg_cuda_kernel.cu'])
