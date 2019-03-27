625051b165f7cce50a7b96479c35e8223e95b5b7
Ziyang Hu <hu.ziyang@cantab.net>
2019-03-27 14:45:45 +0800

    CUDA ext compilation

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
+import os
 from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, CUDAExtension, BuildExtension
+
+ext_mods = [CppExtension(name='torch_asg_native',
+                         sources=['torch_asg/native/torch_asg.cpp'],
+                         extra_compile_args=['-fopenmp'])]
+
+if os.environ.get('CUDA_HOME'):
+    ext_mods.append(CUDAExtension(name='torch_asg_cuda',
+                                  sources=['torch_asg/native/torch_asg_cuda.cpp',
+                                           'torch_asg/native/torch_asg_cuda_kernel.cu']))
 
 setup(
     name='torch_asg',
     version='',
     packages=['torch_asg'],
-    ext_modules=[CppExtension(name='torch_asg_native',
-                              sources=['torch_asg/native/torch_asg.cpp'],
-                              extra_compile_args=['-fopenmp']),
-                 # CUDAExtension(name='torch_asg_cuda',
-                 #               sources=['torch_asg/native/torch_asg_cuda.cpp',
-                 #                        'torch_asg/native/torch_asg_cuda_kernel.cu'])
-                 ],
+    ext_modules=ext_mods,
     cmdclass={'build_ext': BuildExtension},
     url='',
     license='',
--- torch_asg/native/torch_asg_cuda.cpp
+++ torch_asg/native/torch_asg_cuda.cpp
@@ -1,6 +1,17 @@
 //
 // Created by amade on 3/27/2019.
 //
+#include <torch/extension.h>
+//#include <ATen/ATen.h>
+//#include <ATen/Dispatch.h>
+#include <ATen/TensorUtils.h>
+
+#include <numeric>
+#include <type_traits>
+
+#include <iostream>
+#include <vector>
+#include <limits>
 
 namespace torch_asg {
 
@@ -20,3 +31,13 @@ void fcc_loss_backward_gpu() {
 
 }
 }
+
+
+#ifdef TORCH_EXTENSION_NAME
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("fac_loss_gpu", &torch_asg::fac_loss_gpu, "FAC forward");
+    m.def("fac_loss_backward_gpu", &torch_asg::fac_loss_backward_gpu, "FAC backward");
+    m.def("fcc_loss_gpu", &torch_asg::fcc_loss_gpu, "FAC forward");
+    m.def("fcc_loss_backward_gpu", &torch_asg::fcc_loss_backward_gpu, "FAC backward");
+}
+#endif
